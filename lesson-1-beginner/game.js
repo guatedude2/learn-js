4d83564144c19dc5a0e59b89deedf965eaf24bdf
Alejandro Gonzalez <jgonzalezsole@appnexus.com>
2016-11-09 14:59:05 -0500

    Test

--- lesson-1-beginner/game.js
+++ lesson-1-beginner/game.js
@@ -35,15 +35,9 @@ class Game {
       return -1;
     }
 
-    if (this.board[pos - 1] !== ' ') {
-      console.log('Spot has already been marked.');
-    } else if (this.playerTurn === 1) {
-      this.board[pos - 1] = 'X';
-      this.playerTurn = 2;
-    } else {
-      this.board[pos - 1] = 'O';
-      this.playerTurn = 1;
-    }
+    //TODO: Check if pos is valid
+    //TODO: Mark board
+    //TODO: Change turns
 
     if (this.checkWinner('X')) {
       return 1;
@@ -60,21 +54,8 @@ class Game {
     // 012
     // 345
     // 678
-    const b = this.board;
 
-    return (
-      // Check Horizontal
-      (b[0] === mark && b[0] === b[1] && b[1] === b[2]) ||
-      (b[3] === mark && b[3] === b[4] && b[4] === b[5]) ||
-      (b[6] === mark && b[6] === b[7] && b[7] === b[8]) ||
-      // Check Vertical
-      (b[0] === mark && b[0] === b[3] && b[3] === b[6]) ||
-      (b[1] === mark && b[1] === b[4] && b[4] === b[7]) ||
-      (b[2] === mark && b[2] === b[5] && b[5] === b[8]) ||
-      // Check Diagonals
-      (b[0] === mark && b[0] === b[4] && b[4] === b[8]) ||
-      (b[2] === mark && b[2] === b[4] && b[4] === b[6])
-    );
+    //TODO: Check winner
   }
 };
 
